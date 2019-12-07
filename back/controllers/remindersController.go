63543f02394ffbd7fb0f160454cb440ae8e5ae96
EIMIKI <kou.103.kan@gmail.com>
2019-12-07 17:10:36 +0900

    ダミーデーたを適当に

--- back/controllers/remindersController.go
+++ back/controllers/remindersController.go
@@ -8,21 +8,37 @@ import (
 )
 
 type resposeGetReminders struct {
+	Remindrers []responseGetReminder `json:"reminders"`
+}
+
+type responseGetReminder struct {
 	ID       int       `json:"id"`
 	Title    string    `json:"title"`
+	Body     string    `json:"body"`
 	UserName string    `json:"user_name"`
 	Date     time.Time `json:"date"`
 }
 
-type responseGetReminder struct {
-	ID       int    `json:"id"`
-	Title    string `json:"title"`
-	Body     string `json:"body"`
-	UserName string `json:"user_name"`
-}
-
 func (controller *Controller) GetReminders(c *gin.Context) {
-	c.String(http.StatusOK, "OK", nil)
+	reminders := []responseGetReminder{
+		responseGetReminder{
+			ID:       1,
+			Title:    "hoge",
+			Body:     "hogehoge",
+			UserName: "User1",
+			Date:     time.Date(2020, 5, 20, 23, 59, 59, 0, time.Local),
+		},
+		responseGetReminder{
+			ID:       2,
+			Title:    "hoge2",
+			Body:     "hogehoge2",
+			UserName: "User2",
+			Date:     time.Date(2020, 5, 21, 23, 59, 59, 0, time.Local),
+		},
+	}
+
+	res := resposeGetReminders{reminders}
+	c.JSON(http.StatusOK, res)
 }
 
 func (controller *Controller) PostReminders(c *gin.Context) {
@@ -30,5 +46,29 @@ func (controller *Controller) PostReminders(c *gin.Context) {
 }
 
 func (controller *Controller) GetReminder(c *gin.Context) {
-	c.String(http.StatusOK, "OK", nil)
+	id := c.Param("id")
+	res := responseGetReminder{}
+	if id == "1" {
+		res = responseGetReminder{
+			ID:       1,
+			Title:    "hoge",
+			Body:     "hogehoge",
+			UserName: "User1",
+			Date:     time.Date(2020, 5, 20, 23, 59, 59, 0, time.Local),
+		}
+	}
+
+	if id == "2" {
+		res = responseGetReminder{
+			ID:       2,
+			Title:    "hoge2",
+			Body:     "hogehoge2",
+			UserName: "User2",
+			Date:     time.Date(2021, 5, 20, 23, 59, 59, 0, time.Local),
+		}
+	} else {
+		c.JSON(http.StatusNotFound, nil)
+	}
+
+	c.JSON(http.StatusOK, res)
 }
